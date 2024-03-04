fc56247028477ddead77a7fa18ed6e9ccfe68702
geetha1234567890 <60750875+geetha1234567890@users.noreply.github.com>
2024-03-04 13:23:27 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -97,6 +97,13 @@ resource "aws_security_group" "proj-sg" {
  protocol    = "tcp"
  cidr_blocks = ["0.0.0.0/0"]
   }
+ ingress {
+ description = "Allow port 8085 inbound"
+ from_port   = 8085
+ to_port     = 8085
+ protocol    = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+}
  egress {
  from_port = 0
  to_port = 0
